620c8b4f9ef6d32b32ccc81a291176be7b78150b
Dev-Madhan <maddyclickz24@gmail.com>
2025-10-12 23:09:16 +0530

    Github login session completed in login page.

---
+++ lib/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import {prisma} from "@/lib/db";
+import {env} from "@/lib/env";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+    socialProviders: {
+        github: {
+            clientId: env.AUTH_GITHUB_CLIENT_ID,
+            clientSecret: env.AUTH_GITHUB_SECRET,
+        },
+    },
+});
